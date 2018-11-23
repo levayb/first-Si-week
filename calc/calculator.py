46bb0a115d1cc12143b2c2c0a5ba15613cc5c76c
levayb <levayboldizsar@gmail.com>
2018-11-23 22:07:38 +0100

    we done something

--- calc/calculator.py
+++ calc/calculator.py
@@ -1,6 +1,8 @@
 # Simple calculator
 # 2018.11.23
 
+import os
+
 '''
 Write a calculator script that waits for the user to enter a number, 
 then a sign (plus, minus, multiplication and division), 
@@ -12,3 +14,47 @@ The script should exit when the user enters a letter instead of a number.
 
 Submit your python script file.
 '''
+
+
+def add(number1, number2):
+    result_add = int(number1) + int(number2)
+    return result_add
+
+
+def divede(number1, number2):
+    result_div = int(number1) / int(number2)
+    if number2 == 0:
+        return ZeroDivisionError
+    else:
+        return result_div
+
+
+def subtraction(number1, number2):
+    result_sub = int(number1) - int(number2)
+    return result_sub
+
+
+def multiply(number1, number2):
+    result_mul = int(number1) * int(number2)
+    return result_mul
+
+
+def main():
+    while True:
+        fistnumber = input("Please enter a number: ")
+        operator = input("Please choose an operator(| + | / | - | * |): ")
+        secondnumber = input("Please enter a number: ")
+        if operator == '+':
+            print(fistnumber, '+', secondnumber, '=', add(fistnumber, secondnumber))
+        elif operator == '/':
+            print(fistnumber, '/', secondnumber, '=', divede(fistnumber, secondnumber))
+        elif operator == '-':
+            print(fistnumber, '-', secondnumber, '=', subtraction(fistnumber, secondnumber))
+        elif operator == '*':
+            print(fistnumber, '*', secondnumber, '=', multiply(fistnumber, secondnumber))
+        else:
+            break
+
+
+if __name__ == "__main__":
+    main()
--- mma/max_min_avg.py
+++ mma/max_min_avg.py
@@ -1,3 +1,8 @@
+# max-min-avg app
+# 2018.11.23
+
+import os 
+
 # The story
 # Algorithms are one of the most important things for programmers - 
 # they are the foundations on which we build our programs. 
@@ -18,3 +23,39 @@
 # numbers = [-5, 23, 0, "dupa", -9, 12, 99, [2, 44], None, 105, -43] ** 
 # Update your python script to maintain its previous functionality - **
 # please ignore non-numbers and search for numbers inside nested list!
+
+
+def maximum(number_list):
+    max_number = 0
+    for i in number_list:
+        if i > max_number:
+            max_number = i
+    return max_number
+
+
+def minimum(number_list):
+    min_number = 0
+    for y in number_list:
+        if y < min_number:
+            min_number = y
+    return min_number
+
+
+def average(number_list):
+    avg_number = 0
+    for j in number_list:
+        avg_number = avg_number + j
+    avg_number = avg_number / len(number_list)
+    return avg_number
+
+
+def main():
+    os.system('clear')
+    numbers = [-5, 23, 0, -9, 12, 99, 105, -43]
+    print('The maximum number in list is: ', maximum(numbers))
+    print('\nThe minumum number in list is: ', minimum(numbers))
+    print('\nThe average number of the list is: ', average(numbers))
+
+
+if __name__ == "__main__":
+    main()
