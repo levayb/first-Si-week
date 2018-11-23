e12f9f1702549be1ef38eaef3e9254aa021426ad
levayb <levayboldizsar@gmail.com>
2018-11-23 19:11:13 +0100

    first

---
+++ calc/calculator.py
@@ -0,0 +1,14 @@
+# Simple calculator
+# 2018.11.23
+
+'''
+Write a calculator script that waits for the user to enter a number, 
+then a sign (plus, minus, multiplication and division), 
+then a number again. After the 2nd number input, 
+the script should calculate the addition or subtraction and print it out.
+Then the program should run again with asking for the first number.
+
+The script should exit when the user enters a letter instead of a number.
+
+Submit your python script file.
+'''
